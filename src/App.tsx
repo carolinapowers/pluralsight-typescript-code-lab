cb4290161096f3bc3efa62fe7dbe0db1c89a4964
carolinapowers <carolinapoloni@gmail.com>
2025-08-11 12:35:28 -0400

    Initial commit: Bootstrap React + TypeScript + Vite project with ESLint

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export const App: React.FC = () => {
+    return (
+        <div id="root">
+            <h1>Hello, World!</h1>
+            <p>Welcome to the Vite React TypeScript application.</p>
+        </div>
+    );
+}
